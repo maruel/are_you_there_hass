679fd5e3a79f1c1421232226559234e990d0d201
Marc-Antoine Ruel <maruel@google.com>
2023-02-20 09:55:10 -0500

    make_zip.sh: significantly improve

--- make_zip.sh
+++ make_zip.sh
@@ -3,16 +3,31 @@
 # Use of this source code is governed under the Apache License, Version 2.0
 # that can be found in the LICENSE file.
 
+# minify sources into ./packed/ to then zip this directory.
+
 set -eu
+
+cd "$(dirname $0)"
+
+if ! which minify &> /dev/null; then
+  echo "Didn't find minify, installing."
+  go install github.com/tdewolff/minify/v2/cmd/minify@v2.12.4
+fi
 
 if [ -f are_you_there_hass.zip ]; then
   rm are_you_there_hass.zip
 fi
-zip -9 are_you_there_hass.zip \
-  background.js \
-  common.js \
-  eye-lock-open-outline-128.png \
-  manifest.json \
-  options.css \
-  options.html \
-  options.js
+if [ -d packed ]; then
+  rm -rf packed
+fi
+mkdir packed
+
+minify *.css *.html *.js *.json -o packed
+# Assume the png files stored in git already went through pngcrush.
+cp *.png packed
+zip -9 -q -j are_you_there_hass.zip packed/*
+rm -rf packed
+
+# Display content to confirm it's valid. It sounds silly but I wasted 2 hours in
+# the past because one file was missing from the zip.
+unzip -l are_you_there_hass.zip
